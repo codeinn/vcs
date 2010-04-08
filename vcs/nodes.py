dd80b0f6cf5052f17cc738c2951c4f2070200d7f
Lukasz Balcerzak <lukasz.balcerzak@python-center.pl>
2010-04-08 14:17:07 +0200

    Tests for Node class

--- vcs/nodes.py
+++ vcs/nodes.py
@@ -1,3 +1,5 @@
+import posixpath
+
 from vcs.utils.lazy import LazyProperty
 
 class NodeError(Exception):
@@ -9,22 +11,49 @@ class NodeKind:
 
 class Node(object):
     """
-    Simplest class representing file or directory on repository.  Should work
-    for url with or without trailing slash.
+    Simplest class representing file or directory on repository.
+
+    We assert that if node's kind is DIR then it's url **MUST** have trailing
+    slash (with one exception: root nodes have kind DIR but root node's url is
+    always empty string) and FILE node's url **CANNOT** end with slash.
+    Moreover, node's url cannot start with slash, too, as we oparete on
+    *relative* paths only (this class is out of any context).
     """
 
     def __init__(self, url, kind):
+        if url.startswith('/'):
+            raise NodeError("Cannot initialize Node objects with slash at "
+                "the beginning as only relative paths are supported")
         self.url = url
         self.kind = kind
         self.name = url.rstrip('/').split('/')[-1]
         self.dirs, self.files = [], []
+        if self.is_root() and not self.is_dir():
+            raise NodeError, "Root node cannot be FILE kind"
 
     @LazyProperty
     def parent(self):
         parent_url = self.get_parent_url()
-        if parent:
-            return Node(parent_url)
+        if parent_url:
+            return Node(parent_url, NodeKind.DIR)
         return None
+
+    def _get_kind(self):
+        return self._kind
+
+    def _set_kind(self, kind):
+        if hasattr(self, '_kind'):
+            raise NodeError, "Cannot change node's kind"
+        else:
+            self._kind = kind
+            # Post setter check (url's trailing slash)
+            if self.is_file() and self.url.endswith('/'):
+                raise NodeError, "File nodes' urls cannot end with slash"
+            elif not self.url=='' and self.is_dir() and \
+                    not self.url.endswith('/'):
+                raise NodeError, "Dir nodes' urls must end with slash"
+
+    kind = property(_get_kind, _set_kind)
 
     def __cmp__(self, other):
         """
@@ -32,8 +61,17 @@ class Node(object):
         """
         return cmp(self.name, other.name)
 
+    def __eq__(self, other):
+        for attr in self.__dict__:
+            if self.__dict__[attr] != other.__dict__[attr]:
+                return False
+        return True
+
+    def __nq__(self, other):
+        return not self == other
+
     def __repr__(self):
-        return '<Node "%r">' % self.url
+        return '<Node %r>' % self.url
 
     def __unicode__(self):
         return unicode(self.name)
@@ -49,21 +87,10 @@ class Node(object):
     def get_parent_url(self):
         """
         Returns node's parent url or empty string if node is root.
-
-        Examples::
-          >>> node = Node('', NodeKind.DIR).get_parent_url()
-          ''
-          >>> Node('/some/path', NodeKind.DIR)).get_parent_url()
-          '/some/'
-          >>> Node('/some/path/', NodeKind.DIR)).get_parent_url()
-          '/some/'
-          >>> Node('/some/longer/path/', NodeKind.DIR)).get_parent_url()
-          '/some/longer/'
         """
-        if not self.name:
+        if self.is_root():
             return ''
-        node_parent_url = '/'.join(self.url.split('/')[:-1]) + '/'
-        return node_parent_url
+        return posixpath.dirname(self.url.rstrip('/')) + '/'
 
     def is_file(self):
         """
@@ -78,6 +105,12 @@ class Node(object):
         otherwise.
         """
         return self.kind == NodeKind.DIR
+
+    def is_root(self):
+        """
+        Returns ``True`` if node is a root node and ``False`` otherwise.
+        """
+        return self.url == ''
 
     def get_mimetype(self, content):
         # Use chardet/python-magic/mimetypes?
