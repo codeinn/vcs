2d1028c054665b962fa3d307adfc923ddd528038
Marcin Kuzminski <marcin@python-blog.com>
2010-04-08 02:33:44 +0200

    pip standards refactor

--- vcs/backends/hg.py
+++ vcs/backends/hg.py
@@ -3,27 +3,27 @@
 #
 # Copyright (c) 2010 Marcin Kuzminski,Lukasz Balcerzak.  All rights reserved.
 #
-'''
+"""
 Created on Apr 8, 2010
 
 @author: marcink,lukaszb
-'''
+"""
 from base import BaseRepository
 from mercurial import ui
 from mercurial.localrepo import localrepository
 from mercurial.util import matchdate, Abort
 
 class MercurialRepository(BaseRepository):
-    '''
+    """
     Mercurial repository backend
-    '''
+    """
 
     def __init__(self, repo_path, **kwargs):
         baseui = ui.ui()
         self.repo = localrepository(baseui, path=repo_path)
-        '''
+        """
         Constructor
-        '''
+        """
         
     def get_name(self):
         return self.repo.path.split('/')[-2]
