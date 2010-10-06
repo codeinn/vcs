74ebce002c088b8a5ecf40073db09375515ecd68
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-10-06 17:25:29 +0200

    Removed 'dev' from version

--- vcs/__init__.py
+++ vcs/__init__.py
@@ -2,7 +2,7 @@
 Various Version Control System management abstraction layer for Python.
 """
 
-VERSION = (0, 1, 8, 'dev')
+VERSION = (0, 1, 8)
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
