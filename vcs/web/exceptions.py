de9f848b9493e838111558af349c99f1a86a32a8
Marcin Kuzminski <marcin@python-works.com>
2011-05-19 19:13:17 +0200

    removed obsolete/depraceted web from vcs

--- vcs/web/exceptions.py
+++
@@ -1,7 +0,0 @@
-"""
-Web specific exception classes, derived from ``vcs.exceptions.VCSError``.
-"""
-from vcs.exceptions import VCSError
-
-class RequestError(VCSError):
-    pass
--- vcs/web/simplevcs/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Django pluggable application build on top of ``vcs``.
-
-.. warning::
-   This app is deprecated and would be removed after v0.2.
-"""
-
