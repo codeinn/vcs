c5878e667c241080bb3cf6e9d28a7ead431f9734
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2011-09-25 11:42:05 +0200

    Version bump

--- vcs/__init__.py
+++ vcs/__init__.py
@@ -10,7 +10,7 @@
     :copyright: (c) 2010-2011 by Marcin Kuzminski, Lukasz Balcerzak.
 """
 
-VERSION = (0, 2, 1)
+VERSION = (0, 2, 2, 'dev')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
